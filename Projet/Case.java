6d71eefaee9b5945e8747269e8aa1ba202604955
Ludovic <ludovic.lefebvre@ensc.fr>
2013-12-05 19:08:09 -0500

    Ajout du CR + classes Apprenant et Case
    Apprenant Contient le modèle de l'apprenant. Case les information
    nécessaire pour une case.

---
+++ Projet/Case.java
@@ -0,0 +1,24 @@
+package sudoku;
+
+public class Case
+{
+	public int chiffre; //chiffre définitif mis dans la case
+	
+	public boolean tabHypo[] = new boolean[9]; //false le chiffre ne peut se trouver là
+	
+	
+	public Case()
+	{
+		chiffre = 0;
+		
+		for (int i  = 0; i < 9; i++)
+		{
+			tabHypo[i]=false;
+		}
+	}
+	
+	public boolean CaseRemplie()
+	{
+		return (chiffre != 0);
+	}
+}
