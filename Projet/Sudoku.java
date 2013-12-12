f85e322c7a3fdca94083fe8f68d3766097832a89
Ludovic <ludovic.lefebvre@ensc.fr>
2013-12-12 15:13:35 -0500

    Add main file and Coup class

--- Projet/Sudoku.java
+++ Projet/Sudoku.java
@@ -1,9 +1,29 @@
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+
 public class Sudoku
 {
 	Case matriceSudoku[][] = new Case[9][9];
+	Case matriceAffiche[][] = new Case[9][9];
+	Case matrice[][] = new Case[9][9];
+	
+	List listeCoupStrat1 = new LinkedList();
+	List listeCoupStrat2 = new LinkedList();
+	List listeCoupStrat3 = new LinkedList();
 	
 	public Sudoku()
+	{
+		
+	}
+	
+	public boolean CheckValiditeCoup()
+	{
+		return true;
+	}
+	
+	public void MAJCoupPossible()
 	{
 		
 	}
