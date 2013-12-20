cd2ae5e6c5ad53ba94e9cd06883ab083876cd203
Jules <jules.mozes@ensc.fr>
2013-12-20 17:37:50 -0500

    salut

--- Projet/Tuteur.java
+++ Projet/Tuteur.java
@@ -12,37 +12,37 @@ public class Tuteur
 	private boolean _aDejaVuStrat2 = false;
 	private boolean _aDejaVuStrat3 = false;
 	
-	private String message_Connexion_Bienvenue = "Bienvenue sur SudokuSmartTutor, le systeme tutoriel intelligent pour apprendre a jouer au Sudoku !";
-	private String message_Connexion_Interface = "L'interface de SudokuSmartTutor te permet d'eliminer des hypotheses (ecrites en petites) a l'aide du clic droit sur une case. Tu peux aussi jouer un coup pour entrer une valeur definitive (ecrite en gros) a l'aide du clic gauche. Si tu es bloque, tu peux demander de l'aide avec le bouton Indice.";
+	private String message_Connexion_Bienvenue =      "Bienvenue sur SudokuSmartTutor, le systeme tutoriel intelligent pour apprendre a jouer au Sudoku !";
+	private String message_Connexion_Interface =      "L'interface de SudokuSmartTutor te permet d'eliminer des hypotheses (ecrites en petites) a l'aide du clic droit sur une case. Tu peux aussi jouer un coup pour entrer une valeur definitive (ecrite en gros) a l'aide du clic gauche. Si tu es bloque, tu peux demander de l'aide avec le bouton Indice.";
 	
-	private String message_Principe_Introduction = "Pour commencer, voici le principe de base du jeu du Sudoku :";
-	private String message_Principe_But = "Le but du jeu du Sudoku est de completer la grille, divisee en 9 lignes, 9 colonnes et 9 regions (notees de 1 a 9 selon le sens de lecture occidental), en remplissant chaque case par un chiffre compris entre 1 et 9, de maniere a ce que les regles de bases soient satisfaites.";
-	private String message_Principe_Regles = "Un chiffre dans une case satisfait la regle de la colonne si aucun autre meme chiffre ne se trouve dans la colonne de la case en question. Il en va de meme avec la regle de la ligne et la regle de la region.";
+	private String message_Principe_Introduction =    "Pour commencer, voici le principe de base du jeu du Sudoku :";
+	private String message_Principe_But =             "Le but du jeu du Sudoku est de completer la grille, divisee en 9 lignes, 9 colonnes et 9 regions (notees de 1 a 9 selon le sens de lecture occidental), en remplissant chaque case par un chiffre compris entre 1 et 9, de maniere a ce que les regles de bases soient satisfaites.";
+	private String message_Principe_Regles =          "Un chiffre dans une case satisfait la regle de la colonne si aucun autre meme chiffre ne se trouve dans la colonne de la case en question. Il en va de meme avec la regle de la ligne et la regle de la region.";
 	
-	private String message_Strat0_Introduction = "Comme c'est tout premier Sudoku, voici la strategie initiale, dite des hypotheses simples.";
-	private String message_Strat0_Definition = "Un chiffre est une hypothese pour une case si ce chiffre satisfait les trois regles de bases dans cette case. Il peut y avoir plusieurs hypotheses par cases.";
-	private String message_Strat0_Conseil = "Il est pertinant de chercher a eliminer les hypotheses dans les cases dont la colonne, la ligne ou la region correspondante est le moins vide... Tu as plus de chance de pouvoir y appliquer des strategies par la suite !";
+	private String message_Strat0_Introduction =      "Comme c'est tout premier Sudoku, voici la strategie initiale, dite des hypotheses simples.";
+	private String message_Strat0_Definition =        "Un chiffre est une hypothese pour une case si ce chiffre satisfait les trois regles de bases dans cette case. Il peut y avoir plusieurs hypotheses par cases.";
+	private String message_Strat0_Conseil =           "Il est pertinant de chercher a eliminer les hypotheses dans les cases dont la colonne, la ligne ou la region correspondante est le moins vide... Tu as plus de chance de pouvoir y appliquer des strategies par la suite !";
 	
-	private String message_Strat1_Introduction = "Tu as l'air de bien maitriser le principe des hypotheses ! Pour aller plus loin, voici la premiere strategie, dite de l'hypothese seule visible.";
-	private String message_Strat1_Definition = "Une hypothese est la valeur definitive dans une case si dans cette case se trouve cette hypothese et elle seule.";
-	private String message_Strat1_Conseil = "Tu dois appliquer la premiere strategie des que tu constates que c'est possible avant de continuer a chercher d'autres hypotheses... Et n'oublies pas de mettre a jour tes hypotheses en consequence !";
+	private String message_Strat1_Introduction =      "Tu as l'air de bien maitriser le principe des hypotheses ! Pour aller plus loin, voici la premiere strategie, dite de l'hypothese seule visible.";
+	private String message_Strat1_Definition =        "Une hypothese est la valeur definitive dans une case si dans cette case se trouve cette hypothese et elle seule.";
+	private String message_Strat1_Conseil =           "Tu dois appliquer la premiere strategie des que tu constates que c'est possible avant de continuer a chercher d'autres hypotheses... Et n'oublies pas de mettre a jour tes hypotheses en consequence !";
 	
-	private String message_Strat2_Introduction = "Tu as l'air de bien maitriser le principe de l'hypothese seule visible ! Pour aller plus loin, voici la deuxieme strategie, dite de l'hypothese seule cachee.";
-	private String message_Strat2_Definition = "Une hypothese est la valeur definitive dans une case si cette hypothese ne se trouve pas dans les autres cases de la colonne, de la ligne ou de la region de cette case.";
-	private String message_Strat2_Conseil = "Applique la deuxieme strategie des que tu constates que c'est possible ou bien des que la premiere n'est plus applicable... Tu dois alors te concentrer d'avantage sur les hypotheses !";
+	private String message_Strat2_Introduction =      "Tu as l'air de bien maitriser le principe de l'hypothese seule visible ! Pour aller plus loin, voici la deuxieme strategie, dite de l'hypothese seule cachee.";
+	private String message_Strat2_Definition =        "Une hypothese est la valeur definitive dans une case si cette hypothese ne se trouve pas dans les autres cases de la colonne, de la ligne ou de la region de cette case.";
+	private String message_Strat2_Conseil =           "Applique la deuxieme strategie des que tu constates que c'est possible ou bien des que la premiere n'est plus applicable... Tu dois alors te concentrer d'avantage sur les hypotheses !";
 	
-	private String message_Strat3_Introduction = "Tu as l'air de bien maitriser le principe de l'hypothese seule cachee ! Pour aller plus loin, voici la troisieme strategie, dite des paires d'hypotheses visibles.";
-	private String message_Strat3_Definition = "Si dans deux cases parmi une colonne, un ligne ou une region se trouvent les deux memes hypotheses et elles seules, alors toutes ces mêmes hypothèses dans les autres cases de la colonne, de la ligne ou de la region en question peuvent etre eliminee.";
-	private String message_Strat3_Conseil = "Cette strategie ne permet que d'eliminer des hypotheses. Applique la des que possible...Puis essaye d'appliquer a nouveau les precedentes strategies !";
+	private String message_Strat3_Introduction =      "Tu as l'air de bien maitriser le principe de l'hypothese seule cachee ! Pour aller plus loin, voici la troisieme strategie, dite des paires d'hypotheses visibles.";
+	private String message_Strat3_Definition =        "Si dans deux cases parmi une colonne, un ligne ou une region se trouvent les deux memes hypotheses et elles seules, alors toutes ces mêmes hypothèses dans les autres cases de la colonne, de la ligne ou de la region en question peuvent etre eliminee.";
+	private String message_Strat3_Conseil =           "Cette strategie ne permet que d'eliminer des hypotheses. Applique la des que possible...Puis essaye d'appliquer a nouveau les precedentes strategies !";
 	
-	private String message_Erreur_0 = "Attention ! Tu as fait une erreur. Tu peux l'apercevoir en rouge sur la grille.";
-	private String message_Erreur_12 = "Prends garde ! Tu as fait quelques erreurs. Tu peux les apercevoir en rouge sur la grille.";
-	private String message_Erreur_3 = "Doux Jesus ! Tu as fait plusieurs erreurs. Je te laisse soin de les corriger...";
+	private String message_Erreur_0 =                 "Attention ! Tu as fait une erreur. Tu peux l'apercevoir en rouge sur la grille.";
+	private String message_Erreur_12 =                "Prends garde ! Tu as fait quelques erreurs. Tu peux les apercevoir en rouge sur la grille.";
+	private String message_Erreur_3 =                 "Doux Jesus ! Tu as fait plusieurs erreurs. Je te laisse soin de les corriger...";
 
-	private String message_Hasard_12 = "C'est la bonne valeur, mais aucune strategie ne permet de jouer cela ! Serais-tu en train de jouer au hasard ?";
-	private String message_Hasard_3 = "C'est encore la bonne valeur mais c'est encore du hasard. C'est la derniere fois que je te previens !";
+	private String message_Hasard_12 =                "C'est la bonne valeur, mais aucune strategie ne permet de jouer cela ! Serais-tu en train de jouer au hasard ?";
+	private String message_Hasard_3 =                 "C'est encore la bonne valeur mais c'est encore du hasard. C'est la derniere fois que je te previens !";
 	
-	private String message_Indice_Introduction = "Voici un indice qui te debloquera j'espere.";
+	private String message_Indice_Introduction =      "Voici un indice qui te debloquera j'espere.";
 	private String message_Indice_Strat = "";
 	private String message_Indice_Coup = "";
 	private String message_Indice_Region = "";
@@ -60,14 +60,14 @@ public class Tuteur
 	
 	public void MettreAjourMessages()
 	{
-		message_Indice_Strat = "Pour avancer, tu dois appliquer la strategie dite "+_indiceStrat+". Voici un petit rappel.";
-		message_Indice_Coup = "Il y a une case ou un coup est jouable. Il s'agit de la case situee "+_indiceCoup+".";
-		message_Indice_Region = "Il y a une region ou au moins un coup est possible. Il s'agit de la region numero "+_indiceRegion+".";
-		message_Indice_Zone ="Il y a une zone ou au moins au coup est possible. Il s'agit de la partie "+_indiceZone+" de la grille.";
+		message_Indice_Strat =                        "Pour avancer, tu dois appliquer la strategie dite "+_indiceStrat+". Voici un petit rappel.";
+		message_Indice_Coup =                         "Il y a une case ou un coup est jouable. Il s'agit de la case situee "+_indiceCoup+".";
+		message_Indice_Region =                       "Il y a une region ou au moins un coup est possible. Il s'agit de la region numero "+_indiceRegion+".";
+		message_Indice_Zone =                         "Il y a une zone ou au moins au coup est possible. Il s'agit de la partie "+_indiceZone+" de la grille.";
 		
-		message_Succes_1 = "Bravo ! Tu as applique pour la premiere fois "+_succesStrat+" avec succes !";
-		message_Succes_2 = "Tu as encore une fois bien applique "+_succesStrat+" ! Bien joue !";
-		message_Succes_3 = "Felicitation ! Je crois que tu as compris le principe de "+_succesStrat+". Plus la peine de te feliciter a nouveau !";
+		message_Succes_1 =                            "Bravo ! Tu as applique pour la premiere fois "+_succesStrat+" avec succes !";
+		message_Succes_2 =                            "Tu as encore une fois bien applique "+_succesStrat+" ! Bien joue !";
+		message_Succes_3 =                            "Felicitation ! Je crois que tu as compris le principe de "+_succesStrat+". Plus la peine de te feliciter a nouveau !";
 	}
 	
 	public Tuteur(Sudoku zeSudoku)
